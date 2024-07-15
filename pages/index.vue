e726eb5d7be3d79e4c04122be50a86b9c2fd0c84
Anbraten <anton@ju60.de>
2024-07-15 11:24:31 +0200

    feat: save text to localstorage

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="w-full p-4">
-    <Editor v-model="body" />
+    <Editor v-model="text" />
   </div>
 </template>
 
 <script lang="ts" setup>
-const body = ref(`<h2>
-      Hi there,
-    </h2>
-    <p>
-      this is a basic <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-    </p>
-    <ul>
-      <li>
-        That’s a bullet list with one …
-      </li>
-      <li>
-        … or two list items.
-      </li>
-    </ul>
-    <p>
-      Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-    </p>
+import { useStorage } from '@vueuse/core';
+
+const defaultText = `
+<h2>
+  Hi there,
+</h2>
+<p>
+  this is a basic <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+</p>
+<ul>
+  <li>
+    That’s a bullet list with one …
+  </li>
+  <li>
+    … or two list items.
+  </li>
+</ul>
+<p>
+  Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+</p>
 <pre><code class="language-css">body {
   display: none;
 }</code></pre>
-    <p>
-      I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-    </p>
-    <blockquote>
-      Wow, that’s amazing. Good work, boy! 👏
-      <br />
-      — Mom
-    </blockquote>`);
+<p>
+  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+</p>
+<blockquote>
+  Wow, that’s amazing. Good work, boy! 👏
+  <br />
+  — Mom
+</blockquote>
+`.trim();
+
+const text = useStorage('ai-writer:text', defaultText);
 </script>
